8b192a16c033789d9c25ea943fc38044e650331c
Abhilash Dwibedi <you@example.com>
2025-06-06 14:12:20 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = local_file.gcp_credentials.filename
-}
-
-resource "local_file" "gcp_credentials" {
-  filename = "${path.module}/credentials.json"
-  content  = var.credentials
-}
-
 resource "google_compute_instance" "default" {
-  name         = "minimal-vm-${random_id.instance_suffix.hex}"
+  name         = "minimal-vm-${random_id.suffix.hex}"
   machine_type = "e2-micro"
   zone         = var.zone
 
@@ -22,27 +11,15 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-    access_config {
-      // Ephemeral IP
-    }
+    access_config {} // Ephemeral IP
   }
 
   scheduling {
     preemptible       = true
     automatic_restart = false
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "random_id" "instance_suffix" {
+resource "random_id" "suffix" {
   byte_length = 4
-}
-
-data "google_compute_instance" "vm_data" {
-  name = google_compute_instance.default.name
-  zone = var.zone
-  depends_on = [google_compute_instance.default]
 }
