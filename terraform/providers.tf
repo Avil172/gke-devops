f3506d4c6fa8e2ec4d0b312a4d401fbb514dbe9c
Abhilash Dwibedi <you@example.com>
2025-06-06 14:21:36 +0530

    fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,16 +1,14 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = "~> 4.0"
     }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  credentials = file("sa_key.json")
+  project     = var.project_id
+  region      = var.region
 }
